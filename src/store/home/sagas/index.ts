1bd336ae84acd266fd242a0adeb58b2ca9ec6726
IhorFedorkiv94 <ifedorkiv@gmail.com>
2021-08-11 17:15:16 +0300

    init project

---
+++ src/store/home/sagas/index.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import {ForkEffect, put, takeLatest} from "redux-saga/effects";
+import {GenericAction, PayloadWithCallbacks} from "../../types";
+import {activateHomePageType} from "../actions";
+
+//eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function* workerHomePage(action: GenericAction<PayloadWithCallbacks<boolean>>) {
+  console.log(action, "action from saga");
+  yield put({
+    type: activateHomePageType.SUCCESS,
+    payload: action.payload.data
+  });
+}
+
+export default function* homeSaga(): Generator<ForkEffect<never>, void, unknown> {
+  yield takeLatest(activateHomePageType.INIT, workerHomePage);
+}
